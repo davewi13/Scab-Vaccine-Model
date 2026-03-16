fff9ba41011d9665f9ace5339513e4788de5628d
David Ewing <david.ewing@bioss.ac.uk>
2026-03-16 16:33:07 +0000

    Initial commit of ScabVaccineModel project

---
+++ Twonodesexample.R
@@ -0,0 +1,30 @@
+library(SimInf)
+
+
+compartments <- c("S", "I", "C")
+
+data("nodes", package = "SimInf")
+
+#take just the first two nodes 
+nodes <- nodes[1:2,]
+
+
+u0 <- data.frame(S = c(8,8), I= c(1,1), C = c(rep(0, nrow(nodes))), D = c(rep(0, nrow(nodes))), H= c(rep(0, nrow(nodes))))
+
+d_ik <- distance_matrix(x = nodes$x , y= nodes$y, cutoff = 2500)
+
+set.seed(123)
+
+model <- SISe_sp(u0 = u0, tspan = 1:365, events = NULL, phi = 0,
+                 upsilon = (0.0002519243), gamma = (1/77), alpha = 0.005619795, qprop = (1/2), tau = (1/653), epar = (1/3), beta_t1 = 0.06832791,
+                 beta_t2 = 0.06832791, beta_t3 = 0.06832791, beta_t4 = 0.06832791, end_t1 = 91,
+                 end_t2 = 182, end_t3 = 273, end_t4 = 365, distance = d_ik,
+                 coupling = 0.5642857)
+result <- run(model = model, threads = 1)
+
+prevfarms <- prevalence(model = result, formula = I + C ~ S + I + C, type = "nop" )
+withinnodeprev <- prevalence(model = result, formula = I + C ~ S + I + C, type = "wnp")
+
+
+
+
